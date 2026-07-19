4c128eac66631ab2d8b2f070742573d051d43ea4
kantasantosh <kantasantosh@gmail.com>
2026-07-19 20:40:46 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'grihalaxmi-v2.12.0';
+const CACHE = 'grihalaxmi-v2.13.0';
 const FILES = ['/', '/index.html', '/manifest.json', '/icon.svg'];
 
 self.addEventListener('install', e => {
